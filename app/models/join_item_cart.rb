044f6cbbf1d5b79f5ac716cab5ebca6ae6681dd7
Carole Meney <meneycarole@gmail.com>
2021-03-08 22:59:06 +0100

    Adding JoinItemCart table

---
+++ app/models/join_item_cart.rb
@@ -0,0 +1,4 @@
+class JoinItemCart < ApplicationRecord
+  belongs_to :item
+  belongs_to :cart
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_202357) do
+ActiveRecord::Schema.define(version: 2021_03_08_213630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2021_03_08_202357) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "join_item_carts", force: :cascade do |t|
+    t.integer "quantity"
+    t.bigint "item_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_join_item_carts_on_cart_id"
+    t.index ["item_id"], name: "index_join_item_carts_on_item_id"
   end
 
   create_table "users", force: :cascade do |t|
